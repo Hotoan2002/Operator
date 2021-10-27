1a8c9c0f695880f6e745a86c7c4f269adacd9a01
toan <toanzzz2k2@gmail.com>
2021-10-27 15:00:22 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,53 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("nhap a");
+		int a = sc.nextInt();
+		System.out.println("nhap b");
+		int b = sc.nextInt();
+		System.out.println("nhap c");
+		
+		
+		int tong = a + b;
+		System.out.print("Tong: " + tong);
+		int hieu = a - b;
+		System.out.print("Hieu: " + hieu);
+		int nhan = a * b;
+		System.out.print("Nhan: " + nhan);
+		int chia = a / b;
+		System.out.print("Chia: " +chia);
+		
+		// Gan c
+		boolean c = a > b;
+		System.out.println("so sanh hon: " + c);
+		c = a < b;
+		System.out.println("so sanh nho: " + c);
+		c = a <= b;
+		System.out.println("so sanh be hon bang: " + c);
+		c = b <= a;
+		System.out.println("so sanh lon hon bang: " + c);
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
